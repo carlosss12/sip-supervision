b24c8dc3e61c276254498945404b9a5b4614f01c
carlosss12 <127435745+carlosss12@users.noreply.github.com>
2026-05-11 00:57:36 -0400

    Sincronización de puertos 3000/4000

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
-const PORT = process.env.PORT || 4000;
 const novedadesRoutes = require('./routes/novedades');
 
+app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  next();
-});
 
-// Inyección de rutas modulares
 app.use('/api/actividades', novedadesRoutes);
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`[S.I. PROTECTION] Motor API operativo en puerto ${PORT}`);
